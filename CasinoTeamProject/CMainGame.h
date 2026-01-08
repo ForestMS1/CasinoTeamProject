0a7e41d6904fc0a3df835e07413f9a5a816269fb
daesung <dasung56@gmail.com>
2026-01-08 14:16:10 +0900

    [FEAT] PeekMessage 기반 루프

---
+++ CasinoTeamProject/CMainGame.h
@@ -0,0 +1,16 @@
+#pragma once
+class CMainGame
+{
+public:
+	CMainGame();
+	~CMainGame();
+
+public:
+	void Initialize();
+	void Update();
+	void Late_Update();
+	void Render();
+	void Release();
+private:
+};
+
--- CasinoTeamProject/CasinoTeamProject.cpp
+++ CasinoTeamProject/CasinoTeamProject.cpp
@@ -1,8 +1,10 @@
 ﻿// CasinoTeamProject.cpp : 애플리케이션에 대한 진입점을 정의합니다.
 //
-
+#include "pch.h"
+#include "Define.h"
 #include "framework.h"
 #include "CasinoTeamProject.h"
+#include "CMainGame.h"
 
 #define MAX_LOADSTRING 100
 
@@ -10,6 +12,7 @@
 HINSTANCE hInst;                                // 현재 인스턴스입니다.
 WCHAR szTitle[MAX_LOADSTRING];                  // 제목 표시줄 텍스트입니다.
 WCHAR szWindowClass[MAX_LOADSTRING];            // 기본 창 클래스 이름입니다.
+HWND g_hWnd;
 
 // 이 코드 모듈에 포함된 함수의 선언을 전달합니다:
 ATOM                MyRegisterClass(HINSTANCE hInstance);
@@ -27,6 +30,9 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 
     // TODO: 여기에 코드를 입력합니다.
 
+    // 메모리 누수 탐지
+    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+
     // 전역 문자열을 초기화합니다.
     LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
     LoadStringW(hInstance, IDC_CASINOTEAMPROJECT, szWindowClass, MAX_LOADSTRING);
@@ -41,14 +47,39 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_CASINOTEAMPROJECT));
 
     MSG msg;
+    
+    // 메인게임 생성
+    CMainGame mainGame;
+    mainGame.Initialize();
+
+    DWORD dwTime = GetTickCount();
 
     // 기본 메시지 루프입니다:
-    while (GetMessage(&msg, nullptr, 0, 0))
+    while (true)
     {
-        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+        if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
         {
-            TranslateMessage(&msg);
-            DispatchMessage(&msg);
+            if (msg.message == WM_QUIT)
+                break;
+
+            if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+            {
+                TranslateMessage(&msg);
+                DispatchMessage(&msg);
+            }
+        }
+
+        // 창이 활성화 되어있을때만 업데이트
+        else if (GetActiveWindow() == g_hWnd)
+        {
+            if (dwTime + 10 < GetTickCount())
+            {
+                mainGame.Update();
+                mainGame.Late_Update();
+                mainGame.Render();
+
+                dwTime = GetTickCount();
+            }
         }
     }
 
@@ -76,7 +107,7 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
     wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_CASINOTEAMPROJECT));
     wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
     wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
-    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_CASINOTEAMPROJECT);
+    wcex.lpszMenuName   = NULL;
     wcex.lpszClassName  = szWindowClass;
     wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
 
@@ -97,13 +128,21 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 {
    hInst = hInstance; // 인스턴스 핸들을 전역 변수에 저장합니다.
 
+   RECT rc{ 0, 0, WINCX, WINCY };
+   AdjustWindowRect(&rc, WS_OVERLAPPEDWINDOW, FALSE);
+
    HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
-      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+       CW_USEDEFAULT, 0, // 창 시작 x, y 좌표
+       rc.right - rc.left,
+       rc.bottom - rc.top, // 창 가로, 세로 사이즈
+       nullptr, nullptr, hInstance, nullptr);
 
    if (!hWnd)
    {
       return FALSE;
    }
+
+   g_hWnd = hWnd;
 
    ShowWindow(hWnd, nCmdShow);
    UpdateWindow(hWnd);
